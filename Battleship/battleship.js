87b8163f6f925cc71b04b6fc808062f0f2758162
amaralf <amaralf12@hotmail.com>
2017-01-13 14:51:06 +0100

    Added 2 games

---
+++ Battleship/battleship.js
@@ -0,0 +1,109 @@
+function toggle(){
+  document.getElementById('gameboard').style.display = "block";
+}
+
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false)
+
+//determine grid and square size
+var rows = 10;
+var cols = 10;
+var squareSize= 50;
+
+var gameBoardContainer = document.getElementById("gameboard");
+
+//create grid
+for(i = 0; i < cols; i++){
+  for(j = 0; j < rows; j++){
+    var square = document.createElement("div");
+    gameBoardContainer.appendChild(square);
+    square.id = 's' + j + i; //give each square a code based on its position in the grid
+    //set coordinates
+    var topPos = j * squareSize;
+    var leftPos = i * squareSize;
+    square.style.top = topPos + 'px';
+    square.style.left = leftPos + 'px';
+  }
+}
+
+var hits = 0;
+
+var gB1 = [
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,1,1,1,0,0,0,0,0],
+				[0,1,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,1,0,0,0],
+				[0,0,0,0,0,0,1,0,0,0],
+				[1,1,1,1,0,0,1,1,1,1],
+				[0,0,0,1,0,0,0,0,0,0],
+				[0,0,0,1,0,0,0,0,0,0],
+				[0,0,0,1,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0]
+				]
+var gB2 = [
+				[0,0,0,1,1,1,1,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,1,0,0,0],
+				[0,0,0,0,0,0,1,0,0,0],
+				[1,0,0,0,0,0,1,1,1,1],
+				[1,0,0,0,0,0,0,0,0,0],
+				[1,0,0,1,0,0,0,0,0,0],
+				[1,0,0,1,0,0,0,0,0,0],
+				[1,0,0,0,0,0,0,0,0,0]
+				]
+var gB3 = [
+				[0,0,0,1,1,1,1,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,1,0,0,0,0],
+				[0,0,0,0,0,1,1,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[1,0,0,0,0,0,0,0,0,0],
+				[1,0,0,0,0,0,0,1,0,0],
+				[1,0,0,1,0,0,0,1,0,0],
+				[1,0,0,1,0,0,0,0,0,0],
+				[0,0,0,0,0,0,1,1,0,0]
+				]
+var check = Math.floor(Math.random()*3) + 1;
+
+if (check <= 1){
+  var gameBoard = gB1;
+} else if (check < 2 && check >= 1) {
+  var gameBoard = gB2;
+} else {
+  var gameBoard = gB3;
+}
+
+gameBoardContainer.addEventListener("click", fireMissle, false);
+
+function fireMissle(e) {
+    var row = e.target.id.substring(1,2);
+    var col = e.target.id.substring(2,3);
+
+    if (gameBoard[row][col] > 1) {
+        alert("You're wasting ammo! Stop firing!");
+    }
+
+    if(e.target !== e.currentTarget) {
+      //if you miss
+      if(gameBoard[row][col] == 0) {
+        e.target.style.background = 'lightblue';
+        gameBoard[row][col] = 3;
+      //if you hit
+    } else if(gameBoard[row][col] == 1) {
+        e.target.style.background = 'red';
+        gameBoard[row][col] = 2;
+        hits++;
+        }
+        if(hits == 17) {
+          alert("All enemy battleships have been defeated! You rock!");
+          document.getElementById('gameboard').style.display = "none";
+          window.location.reload();
+        }
+      }
+  e.stopPropagation();
+}
---
+++ TheSnekGame/snekSettings.js
@@ -0,0 +1,8 @@
+var mycanvas = document.getElementById("canvas");
+var ctx = mycanvas.getContext("2d");
+var SnekSize = 10;
+var w = 500;
+var h = 500;
+var score = 0;
+var Snek;
+var food;

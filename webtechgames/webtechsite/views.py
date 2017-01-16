d5af1529d8b93afeba21151c340b93c681aa36f5
amaralf <amaralf12@hotmail.com>
2017-01-16 15:11:18 +0100

    API

--- webtechgames/webtechsite/views.py
+++ webtechgames/webtechsite/views.py
@@ -1,6 +1,9 @@
 from .models import Game, Highscore
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import GameSerializer, HighscoreSerializer
+
 
 # ViewSets define the view behavior.
 class GameViewSet(viewsets.ModelViewSet):
@@ -18,3 +21,19 @@ class HighscoreViewSet(viewsets.ModelViewSet):
     queryset = Highscore.objects.all()
     serializer_class = HighscoreSerializer
 
+@api_view(['GET', 'POST'])
+def game_list(request):
+    """
+    List all games, or create a new one
+    """
+    if request.method == 'GET':
+        games = Game.object.all()
+        serializer = GameSerializer(games, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = GameSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

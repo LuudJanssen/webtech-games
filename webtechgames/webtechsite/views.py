c5f0d6413a032ddefae68e4ee10ece9468c8e0ff
niek <niekschwarz@gmail.com>
2017-01-16 16:39:46 +0100

    Login System

--- webtechgames/webtechsite/views.py
+++ webtechgames/webtechsite/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import generic
+from django views.generic import View
 from .forms import UserForm
 from .models import Game, Highscore
 from rest_framework import viewsets, status
@@ -8,48 +10,48 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import GameSerializer, HighscoreSerializer
 
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'webtechsite/registration_form.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form' : form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request, user)
+                    return redirect('webtechsite:index')
+
+        return render(request, self.template_name, {'form':form})
+
+
+
+
 def logout_user(request):
     logout(request)
     form = UserForm(request.POST or None)
     context = {
         "form": form,
     }
-    return render(request, '/../mockupViews/landing/index.html', context)
+    return render(request, 'webtechsite:index.html', context)
 
-
-def login_user(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return render(request, '/../mockupViews/landing/index.html')
-            else:
-                return render(request, '/../mockupViews/landing/index.html', {'error_message': 'Your account has been disabled'})
-        else:
-            return render(request, '/../mockupViews/landing/index.html', {'error_message': 'Invalid login'})
-    return render(request, '/../mockupViews/landing/index.html')
-
-
-def register(request):
-    form = UserForm(request.POST or None)
-    if form.is_valid():
-        user = form.save(commit=False)
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password']
-        user.set_password(password)
-        user.save()
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return render(request, '/../mockupViews/landing/index.html')
-    context = {
-        "form": form,
-    }
-    return render(request, '/../mockupViews/landing/index.html', context)
 
 # ViewSets define the view behavior.
 class GameViewSet(viewsets.ModelViewSet):

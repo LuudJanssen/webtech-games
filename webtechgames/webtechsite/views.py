d0370ac5418b1db43610a4a9574029e464d8ce1b
amaralf <amaralf12@hotmail.com>
2017-01-16 16:44:34 +0100

    Merge branch 'master' of https://github.com/LuudJanssen/webtech-games
    # Conflicts:
    #	webtechgames/webtechsite/views.py

--- webtechgames/webtechsite/views.py
+++ webtechgames/webtechsite/views.py
@@ -3,9 +3,7 @@ from django.contrib.auth import logout
 from django.shortcuts import render
 from .forms import UserForm
 from .models import Game, Highscore
-from rest_framework import viewsets, status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets,
 from .serializers import GameSerializer, HighscoreSerializer
 
 def logout_user(request):
@@ -66,20 +64,3 @@ class HighscoreViewSet(viewsets.ModelViewSet):
     """
     queryset = Highscore.objects.all()
     serializer_class = HighscoreSerializer
-
-@api_view(['GET', 'POST'])
-def game_list(request):
-    """
-    List all games, or create a new one
-    """
-    if request.method == 'GET':
-        games = Game.object.all()
-        serializer = GameSerializer(games, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = GameSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

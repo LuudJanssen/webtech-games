1a42be5de93b57248ed6b30d00e2e26ffd390715
Many <dennisarreman@live.nl>
2017-01-16 22:54:19 +0100

    Login and Logout systems

--- webtechgames/webtechsite/views.py
+++ webtechgames/webtechsite/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from django.views import generic
@@ -24,37 +25,53 @@ def login_user(request):
             return render(request, 'webtechsite/login.html', {'error_message': 'Invalid login'})
     return render(request, 'webtechsite/login.html')
 
+def register(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'webtechsite/index.html')
+    context = {
+        "form": form,
+    }
+    return render(request, 'webtechsite/registration_form.html', context)
 
-class UserFormView(View):
-    form_class = UserForm
-    template_name = 'webtechsite/registration_form.html'
-
-    def get(self, request):
-        form = self.form_class(None)
-        return render(request, self.template_name, {'form' : form})
-
-    def post(self, request):
-        form = self.form_class(request.POST)
-
-        if form.is_valid():
-
-            user = form.save(commit=False)
-
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user.set_password(password)
-            user.save()
-
-            user = authenticate(username=username, password=password)
-
-            if user is not None:
-
-                if user.is_active:
-                    login(request, user)
-                    return render(request, 'webtechsite/index.html')
-
-        return render(request, self.template_name, {'form':form})
-
+# class UserFormView(View):
+#     form_class = UserForm
+#     template_name = 'webtechsite/registration_form.html'
+#
+#     def get(self, request):
+#         form = self.form_class(None)
+#         return render(request, self.template_name, {'form' : form})
+#
+#     def post(self, request):
+#         form = self.form_class(request.POST)
+#
+#         if form.is_valid():
+#
+#             user = form.save(commit=False)
+#
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             user.set_password(password)
+#             user.save()
+#
+#             user = authenticate(username=username, password=password)
+#
+#             if user is not None:
+#
+#                 if user.is_active:
+#                     login(request, user)
+#                     return render(request, 'webtechsite/index.html')
+#
+#         return render(request, self.template_name, {'form':form})
 
 def logout_user(request):
     logout(request)
